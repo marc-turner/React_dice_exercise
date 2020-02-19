e428fcee96b507a1c869697d5d9f73266d160148
Marc Turner <marc.t1967@gmail.com>
2020-02-18 21:58:39 -0500

    Updates

---
+++ precache-manifest.a0070998088d746c8e098410d2f71db2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "396bc82aa5302748f9ad37a13b81f3d2",
+    "url": "/React_dice_exercise/index.html"
+  },
+  {
+    "revision": "449b0a8fe1b48c28e962",
+    "url": "/React_dice_exercise/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "92d36b4fcf1607d91ba3",
+    "url": "/React_dice_exercise/static/js/2.7cd10d42.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React_dice_exercise/static/js/2.7cd10d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "449b0a8fe1b48c28e962",
+    "url": "/React_dice_exercise/static/js/main.6a30db0a.chunk.js"
+  },
+  {
+    "revision": "36a0dedd137013520762",
+    "url": "/React_dice_exercise/static/js/runtime-main.004ca4d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React_dice_exercise/static/media/logo.5d5d9eef.svg"
+  }
+]);
